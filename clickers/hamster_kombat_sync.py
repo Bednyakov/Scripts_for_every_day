6ddc751bb0e07a18e50c641447871ab59f690f16
Tema <108786888+Bednyakov@users.noreply.github.com>
2024-06-27 23:41:56 +0300

    Update hamster_kombat_sync.py

--- clickers/hamster_kombat_sync.py
+++ clickers/hamster_kombat_sync.py
@@ -1,5 +1,6 @@
 import requests
-from time import time, sleep
+import datetime
+from time import sleep
 from random import randint
 
 # URL для отправки POST-запроса синхронизации
@@ -7,20 +8,20 @@ url = "https://api.hamsterkombat.io/clicker/sync"
 
 # Заголовки запроса
 headers = {
-    "Connection":	"keep-alive",
-    "sec-ch-ua":	'"Chromium";v="122", "Not(A:Brand";v="24", "Android WebView";v="122"',
-    "sec-ch-ua-mobile":	'?1',
-    "User-Agent":	"Mozilla/5.0 (Linux; Android 11; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/122.0.6261.64 Mobile Safari/537.36",
-    "sec-ch-ua-platform":	'"Android"',
-    "Accept":	"*/*",
-    "Origin":	"https://hamsterkombat.io",
-    "X-Requested-With":	"org.telegram.messenger",
-    "Sec-Fetch-Site":	"same-site",
-    "Sec-Fetch-Mode":	"cors",
-    "Sec-Fetch-Dest":	"empty",
-    "Referer":	"https://hamsterkombat.io/",
-    "Accept-Encoding":	"gzip, deflate, br",
-    "Authorization": "Bearer YOUR_ACCESS_TOKEN",  # Замените YOUR_ACCESS_TOKEN на ваш токен авторизации
+    "Connection": "keep-alive",
+    "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Android WebView";v="122"',
+    "sec-ch-ua-mobile": '?1',
+    "User-Agent": "Mozilla/5.0 (Linux; Android 11; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/122.0.6261.64 Mobile Safari/537.36",
+    "sec-ch-ua-platform": '"Android"',
+    "Accept": "*/*",
+    "Origin": "https://hamsterkombat.io",
+    "X-Requested-With": "org.telegram.messenger",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://hamsterkombat.io/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Authorization": "Bearer 171947976400350ltlf5V6tBjLcjr2emX7T0J83YedvDdxNUptO1XRcbitqBDR5DYfD415GYe7QLy429078095",  # Замените YOUR_ACCESS_TOKEN на ваш токен авторизации
 }
 
 def sync_request():
@@ -31,8 +32,8 @@ def sync_request():
         response = session.post(url, headers=headers)
 
         if response.status_code == 200:
-            result = response.json()
-            print(result)
+            current_time = datetime.datetime.now().time()
+            print('Ok: ', current_time)
 
         else:
             print("Ошибка при отправке POST-запроса:", response.status_code, response.text)
